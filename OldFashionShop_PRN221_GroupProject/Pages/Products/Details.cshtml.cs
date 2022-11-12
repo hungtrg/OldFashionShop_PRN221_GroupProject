24a4b8dc0f3051555027a8678c94fd628ef8d267
Truong Hiep Hung <hungjt46@gmail.com>
2022-11-12 23:23:07 +0700

    fix product detail
    fix product detail

--- OldFashionShop_PRN221_GroupProject/Pages/Products/Details.cshtml.cs
+++ OldFashionShop_PRN221_GroupProject/Pages/Products/Details.cshtml.cs
@@ -1,40 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using DataLayer.Models;
 
 namespace OldFashionShop_PRN221_GroupProject.Pages.Products
 {
     public class DetailsModel : PageModel
     {
-        private readonly DataLayer.Models.MyStoreManagementContext _context;
-
-        public DetailsModel(DataLayer.Models.MyStoreManagementContext context)
+        public void OnGet()
         {
-            _context = context;
-        }
-
-        public Product Product { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Product = await _context.Products
-                .Include(p => p.Cat).FirstOrDefaultAsync(m => m.ProductId == id);
-
-            if (Product == null)
-            {
-                return NotFound();
-            }
-            return Page();
         }
     }
 }
