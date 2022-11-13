c3535518be3f451adbfe1bdbb073b1aa224488ee
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2022-11-13 23:25:32 +0700

    binding quantity product details

--- OldFashionShop_PRN221_GroupProject/Pages/Products/Details.cshtml.cs
+++ OldFashionShop_PRN221_GroupProject/Pages/Products/Details.cshtml.cs
@@ -1,3 +1,4 @@
+using DataLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,17 @@ namespace OldFashionShop_PRN221_GroupProject.Pages.Products
 {
     public class DetailsModel : PageModel
     {
+        [BindProperty]
+        public string quantity { get; set; }
         public void OnGet()
         {
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (quantity != null) { }
+            return Page();
         }
     }
 }
