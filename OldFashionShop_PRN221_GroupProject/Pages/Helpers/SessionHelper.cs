38d2064e027e5a98db2296d1c8ea12a2a913f617
Huy Nguyen Anh <kuliss999@gmail.com>
2022-11-14 11:19:35 +0700

    feat: add item to cart

---
+++ OldFashionShop_PRN221_GroupProject/Pages/Helpers/SessionHelper.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace OldFashionShop_PRN221_GroupProject.Helpers
+{
+    public static class SessionHelper
+    {
+
+        public static void SetObjectAsJson(this ISession session, string key, object obj)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(obj));
+        }
+
+        public static T? GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
