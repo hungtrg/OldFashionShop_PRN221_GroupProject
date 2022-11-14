3be8dab52cb00fc0cdaf82c977c10dc6baba5b6c
Truong Hiep Hung <hungjt46@gmail.com>
2022-11-14 11:22:03 +0700

    Create Admin OrderManagement
    -Add OrderManagement

---
+++ OldFashionShop_PRN221_GroupProject/Pages/OrderManagements/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataLayer.Models;
+
+namespace OldFashionShop_PRN221_GroupProject.Pages.OrderManagements
+{
+    public class EditModel : PageModel
+    {
+        private readonly DataLayer.Models.MyStoreManagementContext _context;
+
+        public EditModel(DataLayer.Models.MyStoreManagementContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order =  await _context.Orders.FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            Order = order;
+           ViewData["AccountId"] = new SelectList(_context.Accounts, "AccountId", "AccountId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.OrderId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+          return _context.Orders.Any(e => e.OrderId == id);
+        }
+    }
+}
---
+++ OldFashionShop_PRN221_GroupProject/Pages/OrderManagements/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataLayer.Models;
+
+namespace OldFashionShop_PRN221_GroupProject.Pages.OrderManagements
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataLayer.Models.MyStoreManagementContext _context;
+
+        public IndexModel(DataLayer.Models.MyStoreManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Order> Order { get;set; } = default!;
+
+        public async Task OnGetAsync(string quantity, int id)
+        {
+            if (_context.Orders != null)
+            {
+                Order = await _context.Orders
+                .Include(o => o.Account).ToListAsync();
+
+            }
+        }
+    }
+}
