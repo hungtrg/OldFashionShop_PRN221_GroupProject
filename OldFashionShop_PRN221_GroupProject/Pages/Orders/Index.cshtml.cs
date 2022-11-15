7b1a836510972b22eb3528aaa985793490cd093d
Truong Hiep Hung <hungjt46@gmail.com>
2022-11-15 19:52:07 +0700

    edit order tracking
    edit order management

--- OldFashionShop_PRN221_GroupProject/Pages/Orders/Index.cshtml.cs
+++ OldFashionShop_PRN221_GroupProject/Pages/Orders/Index.cshtml.cs
@@ -15,6 +15,7 @@ namespace OldFashionShop_PRN221_GroupProject.Pages.Orders
         private readonly IOrderRepository orderRepository;
 
         public string search { get; set; }
+        public int? id { get; set; }
 
         public IndexModel(IOrderRepository orderRepository)
         {
@@ -23,19 +24,39 @@ namespace OldFashionShop_PRN221_GroupProject.Pages.Orders
 
         public IList<Order> Order { get; set; } = default;
 
-        public async Task OnGetAsync(string search)
+        public Order updateOrder { get; set; } = default;
+
+
+        public async Task OnGetAsync(string search, int? id)
         {
-            //if (_context.Orders != null)
-            //{
-            //}
+            int accountId = int.Parse(HttpContext.Session.GetString("ACCOUNT_ID"));
             if (!string.IsNullOrEmpty(search))
             {
-                Order = this.orderRepository.SearchOrders(search).ToList();
+                Order = this.orderRepository.SearchOrders(search).Where(o => o.AccountId == accountId).ToList();
             }
             else
             {
-                Order = this.orderRepository.GetOrders().ToList();
+                Order = this.orderRepository.GetOrders().Where(o => o.AccountId == accountId).ToList();
             }
+            
+        }
+
+        public IActionResult OnPost(int? id)
+        {
+            updateOrder = this.orderRepository.GetOrderById((int)id);
+
+            var order = new Order
+            {
+                OrderId = updateOrder.OrderId,
+                AccountId = updateOrder.AccountId,
+                OrderDate = updateOrder.OrderDate,
+                Deleted = true,
+                Note = updateOrder.Note,
+                Status = 0
+            };
+            this.orderRepository.UpdateOrder(order);
+
+            return RedirectToPage("./Index");
         }
     }
 }
