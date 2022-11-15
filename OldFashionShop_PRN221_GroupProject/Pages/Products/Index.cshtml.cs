4b7b99b044dc8281311e35d73d5b6a524a3722b9
Huy Nguyen Anh <kuliss999@gmail.com>
2022-11-15 17:05:48 +0700

    fix: filter product

--- OldFashionShop_PRN221_GroupProject/Pages/Products/Index.cshtml.cs
+++ OldFashionShop_PRN221_GroupProject/Pages/Products/Index.cshtml.cs
@@ -26,23 +26,50 @@ namespace OldFashionShop_PRN221_GroupProject.Pages.Products
 
         public async Task OnGetAsync(string search, string? filter, int? pageIndex)
         {
+            string[] catIds;
+            Categories = this.categoryRepository.GetCategories().ToList();
+            var productsIQ = this.productRepository.GetProducts().ToList();
+            var result = new HashSet<Product>();
             if (search != null)
             {
                 pageIndex = 1;
             }
-            else
+            else if (!String.IsNullOrEmpty(filter))
             {
-                search = filter;
+                if (filter.Length > 1)
+                {
+                    catIds = filter.Split(" ");
+                    for (int i = 0; i < catIds.Length; i++)
+                    {
+                        productsIQ = this.productRepository.SearchProducts(catIds[i]).ToList();
+                        foreach (var item in productsIQ)
+                        {
+                            result.Add(item);
+                        }
+                    }
+                }
+                else
+                {
+                    search = filter;
+                }
+
             }
-            Categories = this.categoryRepository.GetCategories().ToList();
-            var productsIQ = this.productRepository.GetProducts().ToList();
 
             if (!string.IsNullOrEmpty(search))
             {
                 productsIQ = this.productRepository.SearchProducts(search).ToList();
             }
-            var pageSize = Configuration.GetValue("PageSize", 10);
-            Product = await PaginatedList<Product>.CreateAsync(productsIQ, pageIndex ?? 1, pageSize);
+            if (result.Count() != 0)
+            {
+                var pageSize = Configuration.GetValue("PageSize", 10);
+                Product = await PaginatedList<Product>.CreateAsync(result.ToList(), pageIndex ?? 1, pageSize);
+            }
+            else
+            {
+                var pageSize = Configuration.GetValue("PageSize", 10);
+                Product = await PaginatedList<Product>.CreateAsync(productsIQ, pageIndex ?? 1, pageSize);
+            }
+
         }
     }
 }
